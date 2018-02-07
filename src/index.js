488742ce319f1cbf83840a78f44ecec9341a05a9
Drew <32339225+drew23c@users.noreply.github.com>
2018-02-06 23:21:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render } from 'react-dom';
+import axios from 'axios';
+import Style from './Style.css'
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+    }
+  }
+  componentDidMount(){
+    this.getRocket();
+  }
+  getRocket = () =>{    
+    console.log('clicked')
+    axios
+      .get('https://api.spacexdata.com/v2/launches/latest')
+      .then(response=>{
+        this.setState({
+          rName:response.data.rocket.rocket_name,
+          rType:response.data.rocket.rocket_type,
+          ldl:response.data.launch_date_local,
+          lSite:response.data.launch_site.site_name_long,
+          det:response.data.details,
+          flNum:response.data.flight_number,
+          lnchY:response.data.launch_year,
+        })
+        console.log(response.data.rocket)
+      })
+  }
+  render(){
+    const styles={
+      div:{
+        color:'red'
+      }
+    }
+    const BodyStyle = props =>(
+      <div className='body'>{props.children}</div>
+    )
+    const {rName,rType,ldl,lSite,det,flNum,lnchY} = this.state
+    return(
+      <div style={styles.div}>
+      <BodyStyle>
+        <center><h1>SpaceX Tracker</h1></center>
+        <p><b>Flight Number:</b> {flNum}</p>
+        <p><b>Launch Year:</b> {lnchY}</p>
+        <p><b>Rocket Name: </b>{rName}</p>
+        <p><b>Rocket Type: </b>{rType}</p>
+        <p><b>Lauch Date Local: </b>{ldl}</p>
+        <p><b>Lauch Site: </b>{lSite}</p>
+        <p><b>Details: </b>{det}</p>
+        <p><a style={styles.div} href="https://www.youtube.com/watch?v=ws9C_UFvBGc"
+        target={"_blank"}>Video</a></p>
+        <p><a style={styles.div} href="https://www.reddit.com/r/spacex/comments/7li8y2/rspacex_iridium_next_4_official_launch_discussion/"
+          target={"_blank"}>Reddit Launch</a></p>
+      </BodyStyle>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));
